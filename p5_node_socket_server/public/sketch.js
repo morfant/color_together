13093604bf5bfdd139fb4dac589e3076ba184e10
morfant <giy.hands@gmail.com>
2019-05-25 12:09:14 +0900

    Done anyway..

--- p5_node_socket_server/public/sketch.js
+++ p5_node_socket_server/public/sketch.js
@@ -97,7 +97,19 @@ var tsLarge = tsNormal*1.5;
 // var rectColor = ["LightBlue", "LightSlateGray", "DarkSlateGray", "RosyBrown", "Teal"];
 var rectColor = [];
 
+var seq;
+var synths = [];
+var loops = [];
+var notes = ["C3", "D3", "E1", "F3", "G3"];
+var durs = ["8n", "16n", "8n", "16n", "8n"];
+var intervals = ["4n", "4n", "4n", "4n", "4n"];
+var vols = [];
 
+var blinkCoverAlpha = [0, 0, 0, 0, 0];
+
+function blinkRect(idx, dur) {
+    rectColor[idx][2] = 0; // brightness
+}
 //================================ setup() ================================
 function setup() {
 
@@ -134,15 +146,123 @@ function setup() {
     // set stage - bind to key pressed to control manually
     stage = MULTI;
 
+
+            synths[0] = new Tone.Synth();
+            synths[1] = new Tone.AMSynth();
+            synths[2] = new Tone.MembraneSynth();
+            synths[3] = new Tone.FMSynth();
+            synths[4] = new Tone.PluckSynth();
+            vols[0] = new Tone.Volume(-12);
+            vols[1] = new Tone.Volume(-12);
+            vols[2] = new Tone.Volume(-26);
+            vols[3] = new Tone.Volume(-20);
+            vols[4] = new Tone.Volume(-12);
+            synths[0].chain(vols[0], Tone.Master);
+            synths[1].chain(vols[1], Tone.Master);
+            synths[2].chain(vols[2], Tone.Master);
+            synths[3].chain(vols[3], Tone.Master);
+            synths[4].chain(vols[4], Tone.Master);
+
+            loops[0] = new Tone.Loop(function(time) {
+                synths[0].triggerAttackRelease(notes[0], durs[0], time);
+                blinkCoverAlpha[0] = 255;
+            }, "2n");
+
+            loops[1] = new Tone.Loop(function(time) {
+                synths[1].triggerAttackRelease(notes[1], durs[1], time);
+                blinkCoverAlpha[1] = 255;
+            }, "2n");
+
+            loops[2] = new Tone.Loop(function(time) {
+                synths[2].triggerAttackRelease(notes[2], durs[2], time);
+                blinkCoverAlpha[2] = 255;
+            }, "2n");
+
+            loops[3] = new Tone.Loop(function(time) {
+                synths[3].triggerAttackRelease(notes[3], durs[3], time);
+                blinkCoverAlpha[3] = 255;
+            }, "2n");
+
+            loops[4] = new Tone.Loop(function(time) {
+                synths[4].triggerAttackRelease(notes[4], durs[4], time);
+                blinkCoverAlpha[4] = 255;
+            }, "2n");
+
+            /*
+            function triggerSynth(time, note, dur) {
+                console.log("triggerSynth()");
+                console.log(time + "/" + note + "/" + dur);
+                synth.triggerAttackRelease(note, dur, time);
+            }
+
+
+            Tone.Transport.schedule(triggerSynth, 0, 200, 1.0);
+            Tone.Transport.schedule(triggerSynth, 1, 300, 1.0);
+            Tone.Transport.schedule(triggerSynth, 2, 400, 1.0);
+            Tone.Transport.schedule(triggerSynth, 3, 500, 1.0);
+            Tone.Transport.schedule(triggerSynth, 4, 600, 1.0);
+
+            // Tone.Transport.schedule(triggerSynth, 1);
+            // Tone.Transport.schedule(triggerSynth, 2);
+            // Tone.Transport.schedule(triggerSynth, 3);
+            // Tone.Transport.schedule(triggerSynth, 4);
+
+            Tone.Transport.loopEnd = '1m';
+            // Tone.Transport.loop = true;
+            */
+            
+            /*
+            seq = new Tone.Sequence(function(time, note){
+                console.log(time);
+                console.log(note);
+                synth.triggerAttackRelease(note, time);
+            }, ["C3", "D3", "C3", "E3", "G3"], ["4n", "8n", "4n", "8n", "8n"]);
+            */
+
+            /*
+            var note = new Tone.Event(function(time, pitch) {
+                console.log(time);
+                console.log(pitch);
+                synth.triggerAttackRelease(pitch, "16n", time)
+            }, "c2");
+
+            note.set({
+                "loop": true,
+                "loopEnd": "2n"
+            });
+
+
+            Tone.Transport.toggle();
+            note.start(0)
+            note.stop("4m")
+            */
+
+
+            /*
+            Tone.Transport.toggle();
+            var part = new Tone.Part(function(time, pitch){
+                console.log(time);
+
+            synth.triggerAttackRelease(pitch, "1", time);
+                }, [["0", "C#3"], ["1", "G3"], ["2", "G#3"], ["3", "C3"]]);
+
+
+            part.start("0");
+            */
+
+
+
+
 }
 
 
 //================================ draw() ================================
+
 function draw() {
 
     switch(stage) {
         case MULTI:
-            colorMode(HSB);
+            colorMode(RGB);
             background(0);
             stroke(255);
 
@@ -150,7 +270,18 @@ function draw() {
 
 
             for (var i = 0; i < 5; i++) {
+
+                if (blinkCoverAlpha[i] > 0) {
+                    blinkCoverAlpha[i]-=25.5;
+                }
+
+
+                colorMode(HSB);
                 fill(color(rectColor[i][0], rectColor[i][1], rectColor[i][2]));
+                rect(i * width/5, 0, width/5, height);
+
+                colorMode(RGB);
+                fill(255, blinkCoverAlpha[i]);
                 rect(i * width/5, 0, width/5, height);
             }
 
@@ -869,14 +1000,19 @@ socket.on('setBackground', function(_data) {
 
 // ========== 7 ==========
 socket.on('control', function(_data) {
-    console.log(_data);
+    // console.log(_data);
     var id = _data.id;
     var x = _data.x;
     var y = _data.y;
 
 
     rectColor[id - 1][0] = y; // hue
-    rectColor[id - 1][2] = x; // brightness
+    rectColor[id - 1][1] = x; // saturation
+
+    notes[id - 1] = Tone.Frequency(y).toNote();
+    intervals[id - 1] = Tone.TimeBase(round(map(x, 0, 100, 1, 16)), "n").toSeconds();
+    // console.log(intervals[id -1]);
+    loops[id - 1].interval = intervals[id - 1];
 
 });
 
@@ -928,9 +1064,24 @@ function getRandomInt(max) {
 //================================== control function ==================================
 function mouseClicked() {
 
-    img_random_idx = Math.floor((Math.random() * 29));
-    console.log(img_random_idx);
+    // img_random_idx = Math.floor((Math.random() * 29));
+    // console.log(img_random_idx);
     // drawImage = !drawImage;
+
+    Tone.Transport.toggle();
+    console.log("Tone transport toggle");
+    // seq.start(0);
+
+    // for (var i = 0; i < 4; i++) {
+    //     loops[i].start(0);
+    // }
+
+    loops[0].start(0)
+    loops[1].start(0)
+    loops[2].start(0)
+    loops[3].start(0)
+    loops[4].start(0)
+
 
 }
 
